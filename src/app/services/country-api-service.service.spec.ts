248e531ebbd0474bb2a7744148c130808644e25e
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-30 12:44:18 +0000

    Generate service for the country api

---
+++ src/app/services/country-api-service.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CountryApiServiceService } from './country-api-service.service';
+
+describe('CountryApiServiceService', () => {
+  let service: CountryApiServiceService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CountryApiServiceService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});

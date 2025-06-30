0a6e5cf1f50b06b9a0444f41f31dca9d2a1129b5
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-30 23:04:28 +0000

    Configure app.config to add ngrx store and reducer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { countryReducer } from './store/country.reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideStore({ country: countryReducer }),
+    provideStoreDevtools({ maxAge: 25, logOnly: false }),
   ],
 };

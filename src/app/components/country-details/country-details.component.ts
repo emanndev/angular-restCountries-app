b64a71b3bb572e577b7a31d3eab483142f6a48d7
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-01 11:05:03 +0000

    Add methods to handle country details logic

--- src/app/components/country-details/country-details.component.ts
+++ src/app/components/country-details/country-details.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink, ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Country } from '../../models/country.interface';
+import {
+  loadCountryByCode,
+  selectCountry,
+} from '../../store/country.actions';
+import {
+  selectSelectedCountry,
+  selectLoading,
+  selectError,
+} from '../../store/country.selectors';
 
 @Component({
   selector: 'app-country-details',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './country-details.component.html',
-  styleUrl: './country-details.component.scss'
+  styleUrls: ['./country-details.component.scss'],
 })
-export class CountryDetailsComponent {
+export class CountryDetailsComponent implements OnInit {
+  country$: Observable<Country | null>;
+  loading$: Observable<boolean>;
+  error$: Observable<string | null>;
 
+  constructor(
+    private store: Store,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.country$ = this.store.select(selectSelectedCountry);
+    this.loading$ = this.store.select(selectLoading);
+    this.error$ = this.store.select(selectError);
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      const code = params['code'];
+      if (code) {
+        this.store.dispatch(loadCountryByCode({ code }));
+      }
+    });
+  }
+
+  goBack() {
+    this.router.navigate(['/countries']);
+  }
+
+  selectBorderCountry(code: string) {
+    this.store.dispatch(selectCountry({ code }));
+    this.router.navigate(['/countries', code]);
+  }
 }
